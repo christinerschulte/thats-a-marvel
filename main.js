7f35c4339daa7d117b950d76d551a14662c0b016
Christine Schulte <christinerschulte@gmail.com>
2018-05-16 21:38:22 -0400

    Store input movies in array

--- main.js
+++ main.js
@@ -4,6 +4,9 @@ const form = document.querySelector('form')
 //create single list
 const list = document.createElement('ul')
 
+//create array
+const movieArray = []
+
 //make list item function
 const createListItem = listItem =>{
     const item = document.createElement('li')
@@ -12,10 +15,12 @@ const createListItem = listItem =>{
     return item
 }
 
-//add items to list function
+//add items to list function & array
 const addToList = item =>{    
     const listItem = createListItem(item)
     list.appendChild(listItem)
+
+    movieArray.push(item)
 
     return list
 }
