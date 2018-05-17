96d494eb730a13badd2a643b22260559b930e1aa
Christine Schulte <christinerschulte@gmail.com>
2018-05-16 21:32:47 -0400

    Make function to add items to list on submit

--- main.js
+++ main.js
@@ -1,5 +1,10 @@
+//get form
 const form = document.querySelector('form')
 
+//create single list
+const list = document.createElement('ul')
+
+//make list item function
 const createListItem = listItem =>{
     const item = document.createElement('li')
     item.textContent = listItem
@@ -7,11 +12,21 @@ const createListItem = listItem =>{
     return item
 }
 
+//add items to list function
+const addToList = item =>{    
+    const listItem = createListItem(item)
+    list.appendChild(listItem)
+
+    return list
+}
+
+//function when form is submitted
 const onSubmit = ev =>{
     ev.preventDefault()
     const movieTitle = ev.target.movieTitle.value
 
-    console.log(createListItem(movieTitle))
+    const movieList = document.querySelector('#movieList')
+    movieList.appendChild(addToList(movieTitle))
 
     ev.target.reset()
 }
