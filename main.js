74e570f58676b42f6dd51e7bcbf1b9fa48c1b07c
Christine Schulte <christinerschulte@gmail.com>
2018-05-16 21:21:34 -0400

    Make createListItem function

--- main.js
+++ main.js
@@ -1,13 +1,19 @@
-//get form input
 const form = document.querySelector('form')
+
+const createListItem = listItem =>{
+    const item = document.createElement('li')
+    item.textContent = listItem
+
+    return item
+}
+
 const onSubmit = ev =>{
     ev.preventDefault()
     const movieTitle = ev.target.movieTitle.value
 
+    console.log(createListItem(movieTitle))
 
-
-    console.log(movieTitle)
+    ev.target.reset()
 }
 
-//addEventListener
 form.addEventListener('submit', onSubmit) 
