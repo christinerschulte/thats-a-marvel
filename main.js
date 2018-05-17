253712244a2f84053b82423031d0e8b3889b957d
Christine Schulte <christinerschulte@gmail.com>
2018-05-16 22:37:37 -0400

    Add delete button that removes list item when clicked

--- main.js
+++ main.js
@@ -10,16 +10,24 @@ const movieArray = []
 //make list item function
 const createListItem = listItem =>{
     const item = document.createElement('li')
-    item.textContent = listItem
+    item.textContent = `${listItem} `
+
+    //add delete button
+    const remove = document.createElement('button')
+    remove.textContent = 'Delete'
+    item.appendChild(remove)
+    remove.addEventListener('click', removeFromList)
 
     return item
 }
 
 //add items to list function & array
-const addToList = item =>{    
+const addToList = item =>{ 
+    //add item to list   
     const listItem = createListItem(item)
     list.appendChild(listItem)
 
+    //add to array
     movieArray.push(item)
 
     return list
@@ -34,6 +42,17 @@ const onSubmit = ev =>{
     movieList.appendChild(addToList(movieTitle))
 
     ev.target.reset()
+}
+
+//delete function
+function removeFromList(ev){
+    //remove from list
+    const removeItem = ev.path[1]
+    const list = ev.path[2]
+    list.removeChild(removeItem)
+
+    return list
+
 }
 
 form.addEventListener('submit', onSubmit) 
