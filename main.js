94f939729f7a5c0c0d9bb76ccf4b9ff8f8a5601c
Christine Schulte <christinerschulte@gmail.com>
2018-05-16 21:05:39 -0400

    Get input from the form

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
-console.log('works')
+//get form input
+const form = document.querySelector('form')
+const onSubmit = ev =>{
+    ev.preventDefault()
+    const movieTitle = ev.target.movieTitle.value
+
+
+
+    console.log(movieTitle)
+}
+
+//addEventListener
+form.addEventListener('submit', onSubmit) 
